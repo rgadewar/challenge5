22903051b113b32a629f608af8711159dc415c59
Rupa Gadewar <rupa@gadewar.com>
2023-05-25 10:59:24 -0500

    Adding local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,60 @@
-var saveButtonEl = $('.saveBtn');
+var saveButtonEl = document.querySelectorAll('.saveBtn');
 var planningAreaEl = $('.form-control');
-var TimeEl = $('.input-group-text');
+var timeEl = $('.input-group-text');
 
-var Anchors = document.getElementsByClassName(".saveBtn");
-console.log("I am here" + Anchors)
-
-
-function handleFormSubmit(event) {
-  event.preventDefault();
-  console.log("I am here")
-  console.log(event.currentTarget.value)
-  var shoppingItem = $('textarea[aria-label="With textarea"]').val();
-  console.log(TimeEl.text())
-  if (!shoppingItem) {
-    alert('Please enter some task!');
-    return;
-  }
-
-  var daySchedule = JSON.parse(localStorage.getItem("daySchedule")) || [];
-  var planning =
-  {
-  time: TimeEl.text(),
-  tasks: shoppingItem,
-    
-};
-  daySchedule.push(planning);
-  localStorage.setItem("daySchedule", JSON.stringify(daySchedule));
-
+var Anchors = $('.saveBtn');
+let numb =  Anchors.length;
+for (var i = 0; i < Anchors.length ; i++)
+{
+    // Anchors[i].on('click', handleFormSubmit);
+console.log(Anchors[i]);
 }
 
 
-saveButtonEl.on('click', handleFormSubmit);
-// $('.delete-item-btn').on('click',deleteButton());
+function handleFormSubmit(event) {   
+  event.preventDefault();
+  var planning =
+  {
+    time: timeEl.text(),
+    tasks: taskItem,  
+  };
+  for (var i = 0; i < Anchors.length ; i++)
+{
+  var taskItem =$(this).siblings(".form-control").val().trim();
+  console.log(taskItem);
+  hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+  console.log(hourSpan);
+  var daySchedule = JSON.parse(localStorage.getItem("daySchedule"));
+  daySchedule.push(planning);
+  localStorage.setItem("daySchedule", JSON.stringify(daySchedule));
+}
+}
 
 
+$('.saveBtn').on('click', handleFormSubmit);
 
 
+$(clearSchedule).on("click", function(event)
+{
+  localStorage.removeItem("daySchedule");
+  window.location.reload();
+});
+
+function init()
+{
+  // When the init function is executed, the code inside renderLastGrade function will also execute
+  renderMessage();
+}
+
+function renderMessage() 
+{
+  
+   for (var i =0; i<daySchedule.length; i++)
+   {
+    // to show current user score
+    $(this).siblings(".input-group-prepend") = daySchedule[i].time
+  
+    $(this).siblings(".form-control") = daySchedule[i].tasks
+    
+  }
+ }
