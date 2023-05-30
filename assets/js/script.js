b25218e46ae493ddc0d5c1cf1ff6eb73ab4ca796
Rupa Gadewar <rupa@gadewar.com>
2023-05-30 11:07:57 -0500

    Refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,10 @@ var planningAreaEl = $('.description');
 function handleFormSubmit(event) {   
   event.preventDefault();
   var taskItem =$(this).siblings(".description").val().trim();
-  hourSpan = $(this).siblings(".text-center").text();
+  hourSpan = $(this).siblings(".description").attr("id");
+  console.log(hourSpan);
+  // var timeTest = $(this).parent().attr("id").split('-');
+  // hourSpan = parseInt(timeTest[1]);
   localStorage.setItem(hourSpan, taskItem);
 }
 
@@ -16,36 +19,57 @@ $(clearSchedule).on("click", function(event)
   window.location.reload();
 });
 
-function init(){
-  var hour = dayjs().hour();
-  pageUpdate();
-  renderMessage();
-}
+// function init(){
+//   var hour = dayjs().hour();
+//   pageUpdate();
+//   renderTimeBlocks();
+// }
 
-function renderMessage() {
-  $(".description").each(function () {
-    var timeTest = parseInt($(this).attr("id"));
+$(document).ready(function () {
+  // var hour = dayjs().hour();
+  pageUpdate();
+  renderTimeBlocks();
+});
+
+function renderTimeBlocks() {
+  $(".time-block").each(function () {
+    // var timeTest = parseInt($(this).attr("id"));
+    // var timeTest1 = parseInt($(this).attr("id"));
+    var timeTest = $(this).attr("id").split('-');
+    timeTest = parseInt(timeTest[1]);
     var hour = dayjs().hour();
     if (hour > timeTest) {
         $(this).addClass("past");
+        $(this).removeClass("future");
+        $(this).removeClass("present");
     } else if (hour < timeTest) {
         $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).removeClass("present");
     } else {
         $(this).addClass("present");
+        $(this).removeClass("past");
+        $(this).removeClass("future");
     }
 });
 }
 
- init();
+
+// document.addEventListener('click', e=>{
+//   if(e.target.matches(".saveBtn")){
+//     console.log("Clicked save button")
+//   }
+// })
+
+//  init();
 
  setInterval(function () {
-  $('#currentDay').text(dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'))
+$('#currentDay').text(dayjs().format('dddd, MMMM D YYYY, h:mm:ss a'))
  },1000);
 
  function pageUpdate(){
-  for (var i =23; i<25; i++)
+  for (var i = 9; i < 20; i++)
    {
-    console.log(i + "pm")
     $("#" + i + "AM" ).val(localStorage.getItem(i + "AM") ) ;
     $("#" + i + "PM" ).val(localStorage.getItem(i + "PM") ) ;
 }
